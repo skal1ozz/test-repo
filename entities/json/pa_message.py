aca1e14f549cb009d3040856cad6ff4764703795
Oleksandr Barabash <skaltmn@gmail.com>
2023-01-05 14:37:28 +0200

    pa_message fixed

--- entities/json/pa_message.py
+++ entities/json/pa_message.py
@@ -1,6 +1,6 @@
 """ PA message to send to Teams """
 from dataclasses import dataclass, field
-from typing import Optional, Union, List, Dict
+from typing import Optional, Union, List, Dict, Any
 
 from entities.json.camel_case_mixin import CamelCaseMixin
 
@@ -10,6 +10,6 @@ class PAMessage(CamelCaseMixin):
     """ PA message Schema """
     conversation_id: str
     tenant_id: str
-    text: Optional[str] = None
-    card: Optional[Dict] = None
-    cards: Optional[List[Dict]] = field(default_factory=list)
+    text: Optional[str]
+    card: Any = None
+    cards: Any = None
